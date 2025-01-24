e70bf4bb6b2db888643881305e1113f9caf0676d
brunovpinheiro <brunovpinheiro@gmail.com>
2025-01-24 11:40:15 -0300

    LP Shopping Finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // SWIPERS
 var carrosselIcones = new Swiper(".carrossel-icons", {
-	direction: "vertical",
+	direction: "horizontal",
 	loop: true,
 	slidesPerView: 1,
 	spaceBetween: 0,
@@ -9,26 +9,25 @@ var carrosselIcones = new Swiper(".carrossel-icons", {
 		delay: 2000,
 		disableOnInteraction: false,
 	},
-});
-
-var slideShoppingsCases = new Swiper(".swiper-desktop-sites", {
-	loop: true,
-	slidesPerView: 1,
-	spaceBetween: 0,
-	centeredSlides: true,
-	autoplay: {
-		delay: 4000,
-		disableOnInteraction: false,
+	breakpoints: {
+		640: {
+			direction: "horizontal",
+		},
 	},
 });
 
-var slideShoppingsCasesMobile = new Swiper(".swiper-mobile-sites", {
+const slideShoppingsCasesMobile = new Swiper(".swiper-mobile-sites", {
 	loop: true,
 	slidesPerView: 1,
-	spaceBetween: 0,
-	centeredSlides: true,
+	allowTouchMove: false,
+});
+
+const slideShoppingsCases = new Swiper(".swiper-desktop-sites", {
+	loop: true,
 	autoplay: {
-		delay: 4000,
-		disableOnInteraction: false,
+		delay: 5000,
+	},
+	thumbs: {
+		swiper: slideShoppingsCasesMobile,
 	},
 });
